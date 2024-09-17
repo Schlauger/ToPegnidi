47a2f174b26fc07d657fe018c15379ac2cf38022
Schlauger <gsavatar99.9@gmail.com>
2024-09-17 17:06:03 +0300

    wearst

--- Start_Game.py
+++ Start_Game.py
@@ -1,24 +1,35 @@
 from bedging_SLUT import *
-import pygame
+import pygame as pyg
 from pygame.locals import *
 from sys import exit
 
 
 def getQuit():
-    for event in pygame.event.get():
+    for event in pyg.event.get():
         if event.type == QUIT:
             return True
-        
-windoSize = (640,640)
+
+
 
 def main():
-    pygame.init()
+    pyg.init()
+    # Initialize variables        
+    windoSize = (640,640)
+    clock = pyg.time.Clock()
+    ticking = 30
     surfaceColor = (144, 0, 255)
-    screen = pygame.display.set_mode(windoSize, DOUBLEBUF, 32)
-    pygame.display.set_caption("Playing in Pygame!")
-    textfont = pygame.font.SysFont("Arial",48)
-    thetext = textfont.render("Hello world!", True,(255,0,0),(255,255,0))
+    screen = pyg.display.set_mode(windoSize, DOUBLEBUF, 32)
+    pyg.display.set_caption("Rogue and King")
+    
+    frst_surface = pyg.Surface(windoSize)
+    frst_surface.fill('Red')
 
+    background = pyg.image.load('assets/background.png')
+    
+
+    #Text
+    textfont = pyg.font.SysFont("Arial",48)
+    thetext = textfont.render("Hello world!", True,(255,0,0),(255,255,0))
     textx = center_Align(windoSize,thetext)
     texty = 40
 
@@ -29,9 +40,16 @@ def main():
         screen.fill((r,g,b))
         screen.blit(thetext,(textx,texty))
         endprogram = getQuit()
-        pygame.display.update()
     
-    pygame.quit()
+        screen.blit(background,(0,windoSize[1]-125))
+
+
+        pyg.display.update()
+
+        #Set while clock
+        clock.tick(60)
+    
+    pyg.quit()
     exit()
 
 if __name__ == "__main__":
---
+++ game_Boltclock.py
@@ -0,0 +1,10 @@
+from bedging_SLUT import *
+import pygame as pyg
+from pygame.locals import *
+from sys import exit
+
+class Artifiscielment: # ideattrezzo
+    def __init__(self) -> None:
+
+        self.clock= pyg.time.Clock()
+        self.clocking = clocking
